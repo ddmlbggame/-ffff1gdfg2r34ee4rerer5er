c06a9d7916ad4bb4359c7be85b61f80be4270c43
20112950 <1214426296@qq.com>
2018-05-07 23:54:13 +0800

    dsfdsfs

--- cengdiexiaorong/Assets/Script/BaseImage.cs
+++ cengdiexiaorong/Assets/Script/BaseImage.cs
@@ -1,5 +1,4 @@
 using System;
-
 public class BaseImage
 {
 	public ImageType baseImageType;
--- cengdiexiaorong/Assets/Script/EditPanel.cs
+++ cengdiexiaorong/Assets/Script/EditPanel.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
-
 public class EditPanel : MonoBehaviour
 {
 	public Transform baseImageGrid;
