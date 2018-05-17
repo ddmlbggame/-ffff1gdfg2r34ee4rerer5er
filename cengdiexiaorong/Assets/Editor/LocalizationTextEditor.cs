c290e7b9c74cb02d5712cf5e925e40338ad10100
20112950 <1214426296@qq.com>
2018-05-18 01:01:52 +0800

    erewrew

---
+++ cengdiexiaorong/Assets/Editor/LocalizationTextEditor.cs
@@ -0,0 +1,122 @@
+﻿
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(localizationText))]
+[HideInInspector]
+public class LocalizationTextEditor : Editor
+{
+	//private SerializedObject serialized_object;
+	//private SerializedProperty localization_languages;
+	//private SerializedProperty lanuages;
+
+	////private int language_count;
+	//private string[] language_names = { "中文", "英文" };
+	//private void OnEnable()
+	//{
+	//	serialized_object = new SerializedObject(target);
+	//	localization_languages = serialized_object.FindProperty("localization_languages");
+	//	if (localization_languages != null)
+	//	{
+	//		//language_count = localization_languages.arraySize;
+	//		if (localization_languages.isArray && localization_languages.arraySize==0)
+	//		{
+	//			localization_languages.arraySize = 2;
+	//			Debug.LogError(localization_languages.arraySize);
+	//		}
+	//	}
+	//}
+
+	//public override void OnInspectorGUI()
+	//{
+	//	base.OnInspectorGUI();
+	//	EditorGUILayout.BeginVertical();
+	//	//language_count = EditorGUILayout.IntField("语言种类", language_count);
+	//	if (localization_languages.isArray)
+	//	{
+	//		for (int i = 0; i < localization_languages.arraySize; i++)
+	//		{
+	//			EditorGUILayout.BeginHorizontal();
+	//			EditorGUILayout.LabelField("中文", GUILayout.Width(40));
+	//			SerializedProperty text = localization_languages.GetArrayElementAtIndex(i);
+	//			text.stringValue = EditorGUILayout.TextArea(text.stringValue, GUILayout.ExpandHeight(true));
+	//			//EditorGUILayout.PropertyField(localization_languages.GetArrayElementAtIndex(i),new GUIContent("中卫"), true, GUILayout.ExpandHeight(true));
+	//			EditorGUILayout.EndHorizontal();
+	//			EditorGUILayout.Space();
+	//			EditorGUILayout.LabelField("");
+	//		}
+	//		//EditorGUILayout.PropertyField(localization_languages.GetArrayElementAtIndex(0), true);
+	//	}
+	//	////当Inspector 面板发生变化时保存数据
+	//	//if (GUI.changed)
+	//	//{
+	//	//	EditorUtility.SetDirty(target);
+	//	//}
+	//	serialized_object.ApplyModifiedProperties();
+	//	EditorGUILayout.EndVertical();
+	//}
+
+	private SerializedObject serialized_object;
+	private SerializedProperty localization_languages;
+
+	//private int language_count;
+	private string[] language_names = { "中文", "英文" };
+	private void OnEnable()
+	{
+		serialized_object = new SerializedObject(target);
+		localization_languages = serialized_object.FindProperty("lanuages");
+		if (localization_languages != null)
+		{
+			//language_count = localization_languages.arraySize;
+			if (localization_languages.isArray && localization_languages.arraySize == 0)
+			{
+				localization_languages.arraySize = 2;
+				Debug.LogError(localization_languages.arraySize);
+			}
+		}
+
+		//if (localization_languages.isArray)
+		//{
+		//	for (int i = 0; i < localization_languages.arraySize; i++)
+		//	{
+		//		SerializedProperty language = localization_languages.GetArrayElementAtIndex(i);
+		//	}
+		//}
+	}
+
+	public override void OnInspectorGUI()
+	{
+		base.OnInspectorGUI();
+		EditorGUILayout.BeginVertical();
+		//language_count = EditorGUILayout.IntField("语言种类", language_count);
+		if (localization_languages.isArray)
+		{
+			for (int i = 0; i < localization_languages.arraySize; i++)
+			{
+				EditorGUILayout.BeginHorizontal();
+				SerializedProperty language = localization_languages.GetArrayElementAtIndex(i);
+				foreach (var item in language)
+				{
+					Debug.Log("----");
+				}
+			
+
+				EditorGUILayout.LabelField("中文", GUILayout.Width(40));
+				SerializedProperty text = localization_languages.GetArrayElementAtIndex(i);
+				text.stringValue = EditorGUILayout.TextArea(text.stringValue, GUILayout.ExpandHeight(true));
+				//EditorGUILayout.PropertyField(localization_languages.GetArrayElementAtIndex(i),new GUIContent("中卫"), true, GUILayout.ExpandHeight(true));
+				EditorGUILayout.EndHorizontal();
+				EditorGUILayout.Space();
+				EditorGUILayout.LabelField("");
+			}
+			//EditorGUILayout.PropertyField(localization_languages.GetArrayElementAtIndex(0), true);
+		}
+		////当Inspector 面板发生变化时保存数据
+		//if (GUI.changed)
+		//{
+		//	EditorUtility.SetDirty(target);
+		//}
+		serialized_object.ApplyModifiedProperties();
+		EditorGUILayout.EndVertical();
+	}
+}
---
+++ cengdiexiaorong/Assets/Script/localizationText.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class localizationText : Text {
+	[HideInInspector]
+	public string[] localization_languages;
+	[HideInInspector]
+	public LocalizationLanguage[] lanuages;
+}
