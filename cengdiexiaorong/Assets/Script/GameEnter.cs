25b61066c489ee5e0e1fcf912bc07ba5ff0dc9de
20112950 <1214426296@qq.com>
2018-05-19 21:24:12 +0800

    1111

--- cengdiexiaorong/Assets/Script/GameEnter.cs
+++ cengdiexiaorong/Assets/Script/GameEnter.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameEnter : MonoBehaviour {
-
 	void Awake()
 	{
+		localizationText.lange_type = Application.systemLanguage;
 		DontDestroyOnLoad(this.gameObject);
 		UIManager.Instance._Init(this.transform);
 		UIManager.Instance.PushShow(UILogin.Info, true);
--- cengdiexiaorong/Assets/Script/localizationText.cs
+++ cengdiexiaorong/Assets/Script/localizationText.cs
@@ -6,6 +6,45 @@ using UnityEngine.UI;
 public class localizationText : Text {
 	[HideInInspector]
 	public string[] localization_languages;
-	[HideInInspector]
-	public LocalizationLanguage[] lanuages;
+	public static SystemLanguage lange_type;
+	protected override void OnEnable()
+	{
+		base.OnEnable();
+		SetText();
+	}
+
+	public override void SetLayoutDirty()
+	{
+		base.SetLayoutDirty();
+#if UNITY_EDITOR
+		SetText();
+#endif
+	}
+
+	private void SetText()
+	{
+#if UNITY_EDITOR
+		if(localization_languages != null && localization_languages.Length>=2)
+		{
+			if (Application.systemLanguage == SystemLanguage.Chinese)
+			{
+				this.m_Text = localization_languages[0];
+			}
+			else if (Application.systemLanguage == SystemLanguage.English)
+			{
+				this.m_Text = localization_languages[1];
+			}
+		}
+#else
+		if (lange_type == SystemLanguage.Chinese)
+		{
+			this.m_Text = localization_languages[0];
+		}
+		else if (lange_type == SystemLanguage.English)
+		{
+			this.m_Text = localization_languages[1];
+		}
+#endif
+
+	}
 }
